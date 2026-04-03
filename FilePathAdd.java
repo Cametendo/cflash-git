c086bcc2e641368be0980367a305af0a2577e712
Cametendo <cameronmathis08@gmail.com>
2026-04-03 15:12:45 +0200

    Made flasher show user configuration

--- FilePathAdd.java
+++ FilePathAdd.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
 
 public class FilePathAdd {
-    public static void filePath(Scanner UserInput) {
+
+    public static String ImagePath = "";
+
+    protected static String filePath(Scanner UserInput) {
         System.out.println("Please enter the FULL Path of your ISO / Image. ()");
-        String ImagePath = UserInput.nextLine();
+        ImagePath = UserInput.nextLine();
         System.out.println("Using File: " + ImagePath);
+        return ImagePath;
     }
 }
--- Flasher.java
+++ Flasher.java
@@ -1,11 +1,21 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Flasher {
-    publicstatic voic flasher(Scanner UserInput) {
+    public static void flasher(Scanner UserInput) {
+        
+        String input = "";
+        
         System.out.println("The programm wil use the following configuration, do you want to flash with this? (y/n)");
-        System.out.println("    - To be flashed device: " + device);
+        System.out.println("    - To be flashed device: " + StorageDeviceLister.fullPath);
+        System.out.println("    - To be used path: " + FilePathAdd.ImagePath);
+        System.out.println("    - To bed used blocksize: " + BlockSize.blockSizeString);
+        System.out.println("    - To be used oflag: " + OflagHandler.oflagHandleString);
+        input = UserInput.nextLine();
+        if (YesNo.check(input)) {
+            System.out.println("Flashing...");
+        } else {
+            System.out.println("Canceling...");
+            System.exit(0);
+        }
     }
 }
